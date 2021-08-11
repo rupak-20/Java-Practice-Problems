f8d12bdf64be3bf0274f21272a5315ca1112281a
rupak-20 <rupakswami43887@gmail.com>
2021-08-11 20:08:27 +0530

    Fibonacci series upto a number

---
+++ Fibonacci_series.java
@@ -0,0 +1,22 @@
+//  Java program to print Fibonacci series, e.g. 1 1 2 3 5 8 13 ... . up to a given number.
+//  Input: integer n
+//  Output: first n integers in the fibonacci series
+
+import java.util.Scanner;
+public class Fibonacci_series {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        int pre=1;
+        int now=1;
+        System.out.println(1);
+        for(int i=1; i<=n; i++) {
+            System.out.println(now);
+            int temp=pre;
+            pre=now;
+            now = temp+now;
+        }
+        sc.close();
+    }
+}
